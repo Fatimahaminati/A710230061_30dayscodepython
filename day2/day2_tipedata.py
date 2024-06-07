eecf73f80a0fc31e6e622ffb366a8911a356ed68
Fatimahaminati <fatimahaminati@gmail.com>
2024-06-07 08:59:19 +0700

    Create day2_tipedata.py
    day2 tipedatakonversi kilometer

---
+++ day2/day2_tipedata.py
@@ -0,0 +1,10 @@
+'mengkonversi perhitungan jarak dari kilometer menjadi meter'
+
+#memasukkan inputan angka dalama kilometer
+km = int(input('angka dalam kilometer :'))
+
+#masukkan rumus pengubahan jarak kilometer ke meter
+m = km*1000
+
+#menampilkan hasil 
+print('jika diubah dalam meter menjadi :',m)
