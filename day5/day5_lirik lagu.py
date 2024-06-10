76a06628688c000dac52b08306b03b40628f3464
Fatimahaminati <fatimahaminati@gmail.com>
2024-06-10 07:49:47 +0700

    Create day5_lirik lagu.py
    day5 lirik lagu anak bebek_perulangan

---
+++ day5/day5_lirik lagu.py
@@ -0,0 +1,9 @@
+#masukkan jumlah anak bebek
+jumlah_anak_bebek = int(input('Maukkan jumlah anak bebek :'))
+
+#loop untuk menampilkan lirik lagu
+for i in range(jumlah_anak_bebek, 0, -1):
+    if i > 1:
+        print(f'Anak bebek turun {i}, mati satu tinggal {i - 1}')
+    else:
+        print('Anak bebek turun 1, mati satu tinggal induknya')
