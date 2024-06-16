fe2c7c970d5b5580add95e19f20d3c1b19de9d7b
Fatimahaminati <fatimahaminati@gmail.com>
2024-06-16 07:49:14 +0700

    Create day 11_exceptionhandling.py
    day 11 mencoba exception handling

---
+++ day 11/day 11_exceptionhandling.py
@@ -0,0 +1,8 @@
+try:
+    bilangan = int(input("Masukkan bilangan bulat: "))
+    kuadrat = bilangan ** 2
+    print(f"Kuadrat dari {bilangan} adalah {kuadrat}")
+except:
+    print("Input yang dimasukkan tidak valid!")
+finally:
+    print("Program selesai")
